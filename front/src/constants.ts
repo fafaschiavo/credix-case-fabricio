0f33d946b3dd55dc1a9e6257c0478bcb212af7ae
fafaschiavo <fayschiavo@gmail.com>
2025-02-18 22:46:18 -0300

    small fix to remove trailing slash

--- front/src/constants.ts
+++ front/src/constants.ts
@@ -2,9 +2,9 @@ const currentDomain = window.location.hostname;
 
 let apiBaseUrl: string;
 if (currentDomain === 'localhost') {
-  apiBaseUrl = 'http://localhost:8000/';
+  apiBaseUrl = 'http://localhost:8000';
 } else {
-  apiBaseUrl = 'https://api.credix.pixelbreeders.com/';
+  apiBaseUrl = 'https://api.credix.pixelbreeders.com';
 }
 
 export const API_BASE_URL: string = apiBaseUrl;
